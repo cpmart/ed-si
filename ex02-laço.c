4b005bb8fc5448594c936d2495acb067a5081af2
cpmart <cpmart@gmail.com>
2022-09-05 11:28:44 -0300

    ex02 - completo

--- ex02-laço.c
+++ ex02-laço.c
@@ -2,13 +2,29 @@
 
 int main()
 {
-    unsigned int fatorial;
-    for (int valor = 5; valor <= 20; valor += 5)
+    int numero, i;
+    unsigned long fatorial = 1;
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    for (i = numero; i > 1; i--)
     {
-        fatorial = 1;
-        for (int i = 1; i <= valor; i++)
-        {
-            fatorial = fatorial * i;
-        }
+        fatorial *= i;
     }
+    printf("FOR - Fatorial de %d: %lu\n", numero, fatorial);
+    i = numero;
+    fatorial = 1;
+    while (i > 1)
+    {
+        fatorial *= i--;
+    }
+    printf("WHILE - Fatorial de %d: %lu\n", numero, fatorial);
+    i = numero;
+    fatorial = 1;
+    if (numero > 1)
+        do
+        {
+            fatorial *= i--;
+        } while (i > 1);
+    printf("DO WHILE - Fatorial de %d: %lu\n", numero, fatorial);
 }
